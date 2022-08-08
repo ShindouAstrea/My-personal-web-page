787a5e9aeb9e07d3b9b7d3eb9c091cfa8024b2a4
Jose Silva <jose.silva.a@mail.pucv.cl>
2022-08-08 19:41:37 -0400

    Diseño de tarjeta para herramientas de desarrollo

---
+++ src/components/DevToolsCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Col,Row,Container,Button,Card} from 'react-bootstrap';
+const devTools = require('../data/herramientas.json');
+export default function DevToolsCard(){
+    return(
+        <Container>
+            <Row>
+            <h1>Frameworks</h1>
+            {
+            devTools.map((item,index)=>(
+              <Col xs="auto" md ={6} lg={6}key= {index} className="mb-4">
+               <Card border="dark">
+                        <Card.Body>
+                            <Card.Title>{item.name}</Card.Title>
+                            <Card.Text>{item.description}</Card.Text>
+                            <Button variant="outline-success" href={item.link}>Ir a  Google play</Button>
+                        </Card.Body>
+                </Card>
+              </Col>
+            ))
+          }
+            </Row>
+        </Container>
+    )
+
+}
