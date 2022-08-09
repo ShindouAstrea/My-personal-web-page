7305a9c7de5e990b6f0c133b7013ef4ee32444d5
Jose Silva <jose.silva.a@mail.pucv.cl>
2022-08-09 01:32:21 -0400

    Logo de mantenimiento

--- src/components/views/HomePage.jsx
+++ src/components/views/HomePage.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import {Container,Row} from 'react-bootstrap' ;
+import '../../styles/Engranaje.css' ;
+import tuerca from '../../assets/tuerca.svg'
 
 function HomePage(){
     
     return(
         <Container>
             <Row>
-                <h1> Nose que poner acá </h1>
+                <h1> Este Sitio web sigue en construccion </h1>
                 <br></br>
+                <img
+                className="engranaje"
+                src={tuerca}
+                alt="tuerca"
+                />
             </Row>
 
         </Container>
