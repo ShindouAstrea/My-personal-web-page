72e4c1ba358d72d4d32eb707583393fcf78c42ee
Jose Silva <jose.silva.a@mail.pucv.cl>
2022-08-11 01:07:45 -0400

    Se incorporan iconos y se cambia el diseño

--- src/components/views/SocialNetWorks.jsx
+++ src/components/views/SocialNetWorks.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Container ,Col,Row} from 'react-bootstrap';
 import SocialNetWorksCard from '../SocialNetworksCard' ;
 function SocialNetWorks(){
     return(
-        <SocialNetWorksCard/>
+        <Container>
+            <Row>
+                <div style={{paddingTop: '100px'}}>
+                    <h1>Redes Sociales</h1>
+                    <div style={{paddingTop:'50px'}}>
+                    <Col>
+                        <SocialNetWorksCard/>
+                    </Col>
+                    </div>
+                </div>
+            </Row>
+
+        </Container>
+       
     )
 }
 export default SocialNetWorks
