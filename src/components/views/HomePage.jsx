11886a32672e6801ea3629fff1914d6fc0e72e35
Jose Silva <jose.silva.a@mail.pucv.cl>
2022-10-11 19:14:47 -0400

    New style

--- src/components/views/HomePage.jsx
+++ src/components/views/HomePage.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {Container,Row,Col} from 'react-bootstrap' ;
 import '../../styles/Engranaje.css' ;
-import tuerca from '../../assets/tuerca.svg'
+import '../../styles/gorda.css'
+import tuerca from '../../assets/tuerca.svg';
+import gorda from '../../assets/gorda.jpg';
 
 function HomePage(){
     
@@ -10,14 +12,21 @@ function HomePage(){
             <Row>
                 <div style={{paddingTop:'100px'}}>
 
-                <Col>
+                <Col lg="0">
                     <h1> Este Sitio web sigue en construccion </h1>
                     <br></br>
                     <img
-                    className="engranaje"
-                    src={tuerca}
-                    alt="tuerca"
-                    />
+                    className='formato'
+                    src={gorda}
+                        alt=""> 
+                    </img>
+                    
+                    
+                </Col >
+                <Col >
+                    <div >
+                            <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi est voluptates dolor numquam laudantium quaerat veniam ea doloremque odio iste esse minus inventore dolores accusamus eum, sint aliquam, ut sed?</h2>
+                        </div>
                 </Col>
                 </div>
 
